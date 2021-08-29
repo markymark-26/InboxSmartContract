b6b475ab6197b55d1b5f058119a60194f1366a4f
Mark Rymsza <mrymsza26@gmail.com>
2021-08-29 12:49:53 -0500

    finished inbox smart contract project

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'embrace still lion curtain trumpet multiply melt soon daughter rookie middle shock',
+  'https://rinkeby.infura.io/v3/112751453665467186b9cce0a31525c3'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
